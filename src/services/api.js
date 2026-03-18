40487bda21806650770169fab59c0e212d4c3fee
Dipankar049 <dipg543@gmail.com>
2026-03-18 19:46:56 +0530

    fix: base url in apk release

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: process.env.EXPO_PUBLIC_API_URL,
+  baseURL: process.env.EXPO_PUBLIC_API_URL || "https://habit-tracker-nz8s.onrender.com/api",
   timeout: 10000,
 });
 
